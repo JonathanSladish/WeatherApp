e94319ba922232d0c9dbe4a94b2794c31bff8f01
JonathanSladish <75808482+JonathanSladish@users.noreply.github.com>
2021-03-08 14:11:53 -0600

    Initial commit

---
+++ src/main/java/com/tts/WeatherApp/models/Request.java
@@ -0,0 +1,9 @@
+package com.tts.WeatherApp.models;
+
+
+import lombok.Data;
+
+@Data
+public class Request {
+    private String zipCode;
+}
---
+++ src/main/java/com/tts/WeatherApp/services/WeatherService.java
@@ -0,0 +1,18 @@
+package com.tts.WeatherApp.services;
+
+
+import com.tts.WeatherApp.models.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+    @Value("${api_key}")
+    private String apiKey;
+
+    public Response getForecast(String zipCode) {
+        return null;
+    }
+}
+
+
