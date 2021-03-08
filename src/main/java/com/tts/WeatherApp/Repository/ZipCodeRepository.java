c31520b00fecdb6801ca328b109034bab35d4bd5
JonathanSladish <75808482+JonathanSladish@users.noreply.github.com>
2021-03-08 16:03:17 -0600

    updated

---
+++ src/main/java/com/tts/WeatherApp/Repository/ZipCodeRepository.java
@@ -0,0 +1,23 @@
+package com.tts.WeatherApp.Repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tts.WeatherApp.models.ZipCodes;
+
+@Repository
+public interface ZipCodeRepository extends CrudRepository<ZipCodes, Long> {
+
+    ZipCodes findByZipCode(String zip);
+
+    @Query(value = "SELECT ZIP_CODE,FROM ZIP_CODES ORDER BY CREATED_AT DESC LIMIT 10",
+            nativeQuery = true)
+    List<String> findMostRecent();
+
+
+
+    }
--- src/main/java/com/tts/WeatherApp/services/WeatherService.java
+++ src/main/java/com/tts/WeatherApp/services/WeatherService.java
@@ -1,17 +1,58 @@
 package com.tts.WeatherApp.services;
 
+        import java.util.List;
 
-import com.tts.WeatherApp.models.Response;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.stereotype.Service;
+        import org.springframework.web.client.HttpClientErrorException;
+        import org.springframework.web.client.RestTemplate;
+
+        import com.tts.WeatherApp.models.Response;
+        import com.tts.WeatherApp.models.ZipCodes;
+        import com.tts.WeatherApp.Repository.ZipCodeRepository;
 
 @Service
 public class WeatherService {
     @Value("${api_key}")
     private String apiKey;
+    @Autowired
+    private ZipCodeRepository zipCodeRepository;
+
 
     public Response getForecast(String zipCode) {
-        return null;
+        createZip(zipCode);
+        String url = "http://api.openweathermap.org/data/2.5/weather?zip=" +
+                zipCode + "&units=imperial&appid=" + apiKey;
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            return restTemplate.getForObject(url, Response.class);
+        }
+        catch (HttpClientErrorException ex) {
+            Response response = new Response();
+            response.setName("error");
+            return response;
+        }
     }
 
-}
+    public ZipCodes findByZipCode(String zip) {
+        return zipCodeRepository.findByZipCode(zip);
+
+    }
+
+    public List<String> findMostRecent(){
+        return zipCodeRepository.findMostRecent();
+    }
+
+    public void createZip(String zip) {
+        ZipCodes zipCode = new ZipCodes();
+        zipCode.setZipCode(zip);
+
+        zipCodeRepository.save(zipCode);
+
+
+        }
+    }
+
+
+
