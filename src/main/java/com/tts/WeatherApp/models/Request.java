f8bb8a4e65345ac87150ed1fb5d1994c2199f546
JonathanSladish <75808482+JonathanSladish@users.noreply.github.com>
2021-03-08 14:59:32 -0600

    updated

--- src/main/java/com/tts/WeatherApp/models/Request.java
+++ src/main/java/com/tts/WeatherApp/models/Request.java
@@ -6,4 +6,5 @@ import lombok.Data;
 @Data
 public class Request {
     private String zipCode;
+
 }
--- src/main/java/com/tts/WeatherApp/services/WeatherService.java
+++ src/main/java/com/tts/WeatherApp/services/WeatherService.java
@@ -13,6 +13,5 @@ public class WeatherService {
     public Response getForecast(String zipCode) {
         return null;
     }
+
 }
-
-
